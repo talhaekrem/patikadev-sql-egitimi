f0a43f300f21e654e297d65fe0a200077e8729e6
talhaekrem <talha.ekrem.99@gmail.com>
2025-04-07 23:59:29 +0300

    ödev title düzeltme

--- odev5.sql
+++ odev5.sql
@@ -1,4 +1,4 @@
---patikadev SQL eğitimi. SQL Temelleri> Ödev5
+--patikadev SQL eğitimi. SQL Temelleri II > Ödev5
 
 --film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
 SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5;
